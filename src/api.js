25f6816d0ddd5fb0406e47162458f605f20e493c
Olaitan1 <holaitan4@gmail.com>
2023-01-26 08:59:59 +0100

    weather app updated

--- src/api.js
+++ src/api.js
@@ -1,12 +1,11 @@
-const axios = require("axios");
-
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "9381494c8emsh0d76cf772de4994p1d739ajsnb6acc3136ddf",
+    "X-RapidAPI-Key": "9381494c8emsh0d76cf772de4994p1d739ajsnb6acc3136ddf", // enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-export const geoApiUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions"
-
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "77450ad265c79d75f31a82d508c81c9d"; // enter your key from openweather API
